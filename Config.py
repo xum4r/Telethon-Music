49f24b68182570afc307c3778cc0d4fb9f6a43f8
xum4r <113111350+xum4r@users.noreply.github.com>
2022-09-15 01:48:17 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,9 @@
-import os
+Freeimport os
 
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5689148671:AAED6HvS9tTY2ZwNUwBoi7EGe2N8BZojQf4")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
